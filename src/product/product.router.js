284af469f88e96c8d360f3200d7d0ed38663186c
Dima-Khai <d.g.morshch@student.khai.edu>
2024-03-22 22:38:02 +0200

    add request to get user products

--- src/product/product.router.js
+++ src/product/product.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { eq } = require('drizzle-orm');
 const db = require('../db');
-const { products } = require('../db/schema');
+const { users, products } = require('../db/schema');
 const router = express.Router();
 
 router.post('/products', async (request, response) => {
@@ -12,6 +13,14 @@ router.post('/products', async (request, response) => {
 router.get('/products', async (request, response) => {
     const products = await db.query.products.findMany();
     return response.json(products);
+});
+
+router.get('/users/:id/products', async (request, response) => {
+    const { id } = request.params;
+   const userProducts = await db.query.products.findMany({
+       where: eq(products.userId, +id)
+   });
+   return response.json(userProducts);
 });
 
 module.exports = router;
